ae6eac676094c4d0c583d5993fda14b7a772663e
Marcus Turner <maturner@mymail.mines.edu>
2016-02-22 14:36:56 -0700

    Added another cat!

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -5,16 +5,19 @@ import javax.swing.JPanel;
 
 
 public class MyDrawing extends JPanel {
-	private Cat cat;
+	private Cat cat1;
+	private Cat cat2;
 	public MyDrawing() 
 	{
 		// Add critters to the drawing
-		cat = new Cat();
+		cat1 = new Cat();
+		cat2 = new Cat();
 	}
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
-		cat.draw(g, 50, 50);
+		cat1.draw(g, 50, 50);
+		cat2.draw(g, 150, 150);
 	}
 	
 	/**
